4d8cac6a702faf82f77e7bf713aaf10c7d748259
edu-05 <edu-05@edu-01-HP>
2019-09-30 11:49:14 +0900

    랜덤 돈벌기 적용

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,32 +3,20 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 길을 가다가 돈을 세번을 주워야되는 설정으로 프로그램 작성
+		//랜덤함수 이용하여 1000,2000,3000원 랜덤으로 줍는거 코딩해보자!
+		
 		// 돈을 저장 시켜야 할 돈 변수 설정
 		int money = 0;
-		// 3번 반속
-		for (int i = 1; i <= 5; ++i) {
-			// 길을 간다라고 화면에 출력
-			System.out.println("길을 간다");
-			// 돈을 주우면 돈 변수 값이 변경이 되고
-			System.out.println("1000원을 주었다");
-			money = money + 1000;
-			// 얼마가 되었다 출력
-			System.out.println("내 돈은?" + money);
-		}
-		if(money >= 5000) {
-			System.out.println("밥을 먹을수 있다");
-			
-		}else {
-			System.out.println("돈 더 벌어라");
-		}
 		
-		int a=0;
+		//위 기능을 클래스에 메소드로 만들어서 불리
+
 		MyClass mc = new MyClass();
-		money = mc.coin(money);
-		mc.coin2();
-		mc.coin3();
+		money =mc.coinRandom(money);
+	//	money = mc.coin1(money);
+	//	mc.coin2();
+	//	mc.coin3();
 		System.out.println("내 돈은?" + money);
+		
 	
 		
 		
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class MyClass {
 	
@@ -6,7 +7,7 @@ public class MyClass {
 	}
 	
 	// 돈을 넣으면 1000원 더해주는 메소드
-	public void coin1(int money) {
+	public void coin(int money) {
 		System.out.println("1000원을 더 했어요!");
 		}
 		public void coin2() {
@@ -16,14 +17,29 @@ public class MyClass {
 			System.out.println("3000원을 더 했어요!");
 			}
 	
-		public int coin(int money) {
+		public int coin1(int money) {
 			money = money +1000;
 			System.out.println("당신의 돈은?");
 			return money;
+			}
+		public int coinRandom(int money) {
+			Random rnd = new Random();
+			//System.out.println("랜덤 숫자는?" + rnd.nextInt(3));
+			int num = rnd.nextInt(3);
+			//만약에 랜덤 숫자가 0이면 1000원
+			if (num == 0) {
+				money = money + 1000;
+			}
+			//랜덤숫자가 1 이면 2000원
+			else if (num == 1) {
+				money = money + 2000;
+				}
+			//랜덤숫자가 2 이면 3000원
+			else if (num == 2) {
+				money = money + 3000;
+				}
 			
-		
-			
-			
+			return money;
 			
 		}
 }
